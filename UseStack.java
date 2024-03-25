26dbecd02e2acffb331531b199d61f5bc5fd69ed
Mishal Turkane <95625543+mishalturkane@users.noreply.github.com>
2024-03-25 10:31:33 +0530

    Add files via upload

---
+++ UseStack.java
@@ -0,0 +1,19 @@
+package doughts;
+
+public class UseStack {
+    public static void main(String[] args) {
+         Stack st=new Stack(5);
+
+         st.push(20);
+         st.push(30);
+         st.push(40);
+         st.push(50);
+         st.push(10);
+        st.peek();
+
+
+
+
+
+    }
+}
